c11eaafd7193d7788c8305bb32f7adfda27570ec
Fatmaaktar <fatmaaktar214@gmail.com>
2023-04-27 22:25:02 +0300

    btkJavaLessons

---
+++ classesWithAttributes/ProductManager.java
@@ -0,0 +1,10 @@
+package classesWithAttributes;
+
+public class ProductManager {
+    public void Add(Product product) {
+        System.out.println("Urun Eklendi " + product.name);
+    }
+    public void Add2(){
+
+    }
+}

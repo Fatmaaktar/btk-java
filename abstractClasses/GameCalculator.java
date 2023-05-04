6048626cd01604856a2e32d1b6a29c9961e6784c
Fatmaaktar <fatmaaktar214@gmail.com>
2023-05-04 22:50:46 +0300

    btkJavaLessons

---
+++ abstractClasses/GameCalculator.java
@@ -0,0 +1,11 @@
+package abstractClasses;
+
+public class GameCalculator {
+    public void hesapla(){
+        System.out.println("Puaniniz : 100");
+    }
+
+    public void gameOver(){
+        System.out.println("Oyun Bitti");
+    }
+}

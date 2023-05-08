eef7dff0bdbbfef8ac08c0c7ba0eba45386f1cc8
Fatmaaktar <fatmaaktar214@gmail.com>
2023-05-08 21:17:27 +0300

    btkJavaLessons

---
+++ arraylistDemo/Main.java
@@ -0,0 +1,22 @@
+package arraylistDemo;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList sayilar = new ArrayList();
+        System.out.println(sayilar.size());
+        sayilar.add(1);
+        sayilar.add(10);
+        sayilar.add("Ankara");
+        //   System.out.println(sayilar.size()
+        //        sayilar.set(2,100);
+//        sayilar.remove(0);
+//        System.out.println(sayilar.get(0));
+
+        for (Object i : sayilar) {
+            System.out.println(i);
+        }
+
+    }
+}
